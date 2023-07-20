7d005d8c254397ee3efeed891dc623062a899723
Flopiras <Flopiras@yahoo.it>
2023-07-20 17:26:04 +0200

    fix json_decode

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ $database_path = __DIR__ . '/../database/tasks.json';
 $json_data = file_get_contents($database_path);
 
 // rendo il file leggibile
-$tasks = json_decode($json_data);
+$tasks = json_decode($json_data, true);
 
 header('Content-Type: application/json');
 
