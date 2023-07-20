49a7a2812b5181c4f1ca7b59758ef88ce1174648
Flopiras <Flopiras@yahoo.it>
2023-07-20 16:50:11 +0200

    add tasks in jsn

--- api/index.php
+++ api/index.php
@@ -1,22 +1,6 @@
 <?php
 
-$tasks = [
-    [
-        'id' => 1,
-        'text' => 'comprare il latte',
-        'done' => false
-    ],
-    [
-        'id' => 2,
-        'text' => 'dare da mangiare al coccodrillo',
-        'done' => false
-    ],
-    [
-        'id' => 3,
-        'text' => 'preparare la torta',
-        'done' => false
-    ]
-];
+
 
 
 header('Content-Type: application/json');
