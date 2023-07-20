2d185b476a124e24db91624f57269507a0d3aeac
Flopiras <Flopiras@yahoo.it>
2023-07-20 17:43:03 +0200

    add $new_task

--- api/index.php
+++ api/index.php
@@ -8,6 +8,16 @@ $json_data = file_get_contents($database_path);
 // rendo il file leggibile
 $tasks = json_decode($json_data, true);
 
+// preparo una variabile per ricevere i dati in POST
+$new_task_text = $_POST['task'] ?? null;
+
+// se ricevo qualcosa 
+if ($new_task_text) {
+
+    // preparo l'oggetto da pushare
+    $new_task = ['text' = $new_task_text, 'id' = get_new_id()];
+}
+
 header('Content-Type: application/json');
 
 echo json_encode($tasks);
