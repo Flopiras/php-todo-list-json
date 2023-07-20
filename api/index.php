5d237b9b1a822357ba906646f7f4c418396c37f1
Flopiras <Flopiras@yahoo.it>
2023-07-20 16:55:25 +0200

    take tasks from database

--- api/index.php
+++ api/index.php
@@ -1,7 +1,12 @@
 <?php
 
+$database_path = __DIR__ . '/../database/tasks.json';
 
+// prendo il file con le tasks
+$json_data = file_get_contents($database_path);
 
+// rendo il file leggibile
+$tasks = json_decode($json_data);
 
 header('Content-Type: application/json');
 
