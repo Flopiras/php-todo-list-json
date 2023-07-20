dba3d465df163556d9021a8bcde30bda776f2725
Flopiras <Flopiras@yahoo.it>
2023-07-20 16:37:43 +0200

    mod $tasks

--- api/index.php
+++ api/index.php
@@ -1,5 +1,24 @@
 <?php
 
-$tasks = ['comprare il latte', 'dare da mangiare al coccodrillo', 'preparare la torta'];
+$tasks = [
+    [
+        'id' => 1,
+        'text' => 'comprare il latte',
+        'done' => false
+    ],
+    [
+        'id' => 2,
+        'text' => 'dare da mangiare al coccodrillo',
+        'done' => false
+    ],
+    [
+        'id' => 3,
+        'text' => 'preparare la torta',
+        'done' => false
+    ]
+];
+
+
 header('Content-Type: application/json');
+
 echo json_encode($tasks);
